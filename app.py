629858941742ceb02ca9eedc49b742d7dc1a5b89
kim <pur9799@gmail.com>
2022-06-24 16:37:46 +0900

    S2 URL이미지 넣으면 얼굴인식 페이지

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello():
     return render_template('hello.html')
 
+@app.route('/naver')
+def naver():
+    return render_template('naver.html')
+
 @app.route('/a')
 def a():
     return 'Hello,a!'
@@ -25,6 +29,20 @@ def method():
                 AI가 분석한 결과입니다.
                 "<img src='{url}'width='320'height='320'>
                 <img src=/static/img/face.png 'width='320'height='320'>"
+                '''
+
+@app.route('/newsapi', methods=['GET', 'POST'])
+def newsapi():
+    if request.method == 'GET':
+        return "GET으로 전달"
+    else:
+        #뉴스 검색어 받기
+        keyword=request.form["keyword"]
+        #네이버 뉴스 정보 받아오는 코드 실행
+        
+        return f'''
+                유저가 검색한 내용<mark>{keyword}</mark>의
+                뉴스 정보가 나와야 한다.
                 '''
 
 if __name__ == '__main__':
